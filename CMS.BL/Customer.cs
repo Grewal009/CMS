b6cf5126c61134708298a60b04c34f8a0015c18a
Aneel Grewal <aneel.grewal@hotmail.com>
2024-08-19 12:46:05 +0200

    Customer class created in CMS.BL

---
+++ CMS.BL/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CMS.BL;
+
+public class Customer
+{
+    public int CustomerId { get; private set; }
+    public string FirstNane { get; set; }
+    public string LastName { get; set; }
+    public string FullName {
+        get
+        {
+            return LastName + ", " + FirstNane;
+        }
+    }
+    
+    
+}
